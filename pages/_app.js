fcd5289c14fae99f689bf57c8460afa56d504227
Bashir Uddin <uddinbashir1999@gmail.com>
2023-03-26 15:03:23 +0500

    fix: init next project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import LayoutMain from "@/layouts/main.layout";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <LayoutMain>
+      <Component {...pageProps} />
+    </LayoutMain>
+  );
 }
